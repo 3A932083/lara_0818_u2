dd67da6dcaf6ed1a332c1e2107aad5264fb64234
jiang shu yi <3a932083@gm.student.ncut.edu.tw>
2022-11-03 23:20:21 +0800

    練習3-傳遞資料至 view 顯示(新增HelloController,從 Controller 將參數傳遞至 view)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,5 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home',[HomeController::class, 'index'])->name('index.blade.php');
+Route::get('home',[HomeController::class, 'index'])->name('home.index');
+Route::get('home/{name?}',[HelloController::class, 'index'])->name('hello.index');
 
